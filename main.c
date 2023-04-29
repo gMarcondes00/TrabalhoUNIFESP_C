3b1b5822dfa84fdf9e1c7080bdcde0c9388b8e69
gMarcondes00 <g.os.marcondes@gmail.com>
2023-04-29 16:10:51 -0300

    Criação da dinamica de leitura da descrição

--- main.c
+++ main.c
@@ -1,69 +1,134 @@
 #include <stdio.h>
 #include <string.h>
 
-//definir estrutura para todos os livros
+//Definir estrutura para todos os livros
 struct Livro
 {
     int disponivel;
     int vendido;
-    int id;
+    int id; //Código do Livro
     float valor;
     char descricao[200];
 };
 
+struct Livro Matematica;
+struct Livro Probabilidade;
+struct Livro Fisica;
+struct Livro Quimica;
+struct Livro Computacao;
 
-int main() {
-    //Definir Livro de Matematica
-    struct Livro Matematica;
-    Matematica.disponivel = 5;
-    Matematica.vendido = 5;
-    Matematica.id = 1;
-
+//2) Mostrar a descrição da categoria
+void desc(id) {
+    //Path Matematica
     FILE *descMat = fopen("matematica.txt", "r");
     fgets(Matematica.descricao, 200, descMat);
     fclose(descMat);
-
-
-
-    //Definir Livro de Probabilidade e Estatistica
-    struct Livro Probabilidade;
-    Probabilidade.disponivel = 5;
-    Probabilidade.vendido = 5;
-    Probabilidade.id = 1;
-
+    //Path Probabilidade e estatistica
     FILE *descProb = fopen("probabilidade.txt", "r");
     fgets(Probabilidade.descricao, 200, descProb);
     fclose(descProb);
-
-    //Definir Livro de Fisica
-    struct Livro Fisica;
-    Fisica.disponivel = 5;
-    Fisica.vendido = 5;
-    Fisica.id = 1;
-
+    //Path Fisica
     FILE *descFis = fopen("fisica.txt", "r");
     fgets(Fisica.descricao, 200, descFis);
     fclose(descFis);
-
-    //Definir Livro de Quimica
-    struct Livro Quimica;
-    Quimica.disponivel = 5;
-    Quimica.vendido = 5;
-    Quimica.id = 1;
-
+    //Path Quimica
     FILE *descQuim = fopen("quimica.txt", "r");
     fgets(Quimica.descricao, 200, descQuim);
     fclose(descQuim);
-
-    //Definir Livro de Computação
-    struct Livro Computacao;
-    Computacao.disponivel = 5;
-    Computacao.vendido = 5;
-    Computacao.id = 1;
-
+    //Path Computação
     FILE *descComp = fopen("computacao.txt", "r");
     fgets(Computacao.descricao, 200, descComp);
     fclose(descComp);
+
+    switch (id){
+        case 1:
+            printf("%s",Matematica.descricao);
+            break;
+        case 2:
+            printf("%s",Probabilidade.descricao);
+            break;
+        case 3:
+            printf("%s",Fisica.descricao);
+            break;
+        case 4:
+            printf("%s",Quimica.descricao);
+            break;
+        case 5:
+            printf("%s",Computacao.descricao);
+            break;
+
+        default:
+            break;
+    }
+    //Caso queira outra descrição
+    char voltar[4];int idInput2;
+    printf("\nGostaria de ver outra descricao?\n");
+    scanf("%s",voltar);
+    if(!strcmp(voltar, "sim") || !strcmp(voltar, "Sim") || !strcmp(voltar, "SIM")){
+        printf("Qual descricao?");
+        scanf("%d", &idInput2);
+        desc(idInput2);
+    } 
+}
+
+int main() {
+    //Definir Livro de Matematica
+    Matematica.disponivel = 5;
+    Matematica.vendido = 5;
+    Matematica.id = 1;
+    Matematica.valor = 150.00;
+    
+    //Definir Livro de Probabilidade e Estatistica
+    Probabilidade.disponivel = 10;
+    Probabilidade.vendido = 0;
+    Probabilidade.id = 2;
+    Probabilidade.valor = 100.00;
+
+    //Definir Livro de Fisica
+    Fisica.disponivel = 8;
+    Fisica.vendido = 2;
+    Fisica.id = 3;
+    Fisica.valor = 160.00;
+
+    //Definir Livro de Quimica
+    Quimica.disponivel = 7;
+    Quimica.vendido = 3;
+    Quimica.id = 4;
+    Quimica.valor = 160.00;
+
+    //Definir Livro de Computação
+    Computacao.disponivel = 1;
+    Computacao.vendido = 9;
+    Computacao.id = 5;
+    Computacao.valor = 200.00;
+
+    //5)Mantenha o programa em loop
+    char voltar[4];
+    while(strcmp(voltar, "sim") || strcmp(voltar, "Sim") || strcmp(voltar, "SIM")){
+        //1) Mostrar total de unidades e valor de cada categoria
+        printf("Listagem de livros:\n");
+        printf("---------------------------- Disponivel --- Vendido --- Codigo --- Valor\n");
+
+        printf("Matematica:                      %d             %d        %d      R$%.2f\n", Matematica.disponivel, Matematica.vendido, Matematica.id, Matematica.valor);
+        printf("Probabilidade e estatistica:     %d            %d        %d      R$%.2f\n", Probabilidade.disponivel, Probabilidade.vendido, Probabilidade.id, Probabilidade.valor);
+        printf("Fisica:                          %d             %d        %d      R$%.2f\n", Fisica.disponivel, Fisica.vendido, Fisica.id, Fisica.valor);
+        printf("Quimica:                         %d             %d        %d      R$%.2f\n", Quimica.disponivel, Quimica.vendido, Quimica.id, Quimica.valor);
+        printf("Computacao:                      %d             %d        %d      R$%.2f\n\n", Computacao.disponivel, Computacao.vendido, Computacao.id, Computacao.valor);
+
+        //Perguntar se quer sair
+        printf("\nGostaria de sair das compras?\n");
+        scanf("%s",voltar); 
+
+        //Pegar o assunto do livro desejado
+        int idInput;
+        printf("\nTendo a tabela em vista, selecione a descricao do assunto do livro que gostaria com base no Codigo:\n");
+        scanf("%d", &idInput);
+        desc(idInput);
+        
+        //Iniciar processo de compra
+        printf("Gostaria de comprar algum livro?");
+        //scanf("");
+    }
 
     return 0;
 }
